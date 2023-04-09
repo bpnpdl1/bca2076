f60670ac716b609059161a1b0a0a4c8042cb2174
bpnpdl <bipinpaudel6774@gmail.com>
2023-04-09 19:41:17 +0545

    First commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('master')
+
+@section('content')
+<nav>
+    <ul class="menu">
+        <li><a href="">Home</a></li>
+        <li><a href="">Gallery</a></li>
+        <li><a href="">Products</a></li>
+        <li><a href="">Services</a></li>
+    </ul>
+</nav>
+@endsection
---
+++ resources/views/master.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>This is a Page</title>
+
+    <link rel="stylesheet" href="{{ asset('mycss/style.css') }}">
+</head>
+<body>
+    @yield('content')
+    <img src="   " alt="">
+</body>
+</html>
